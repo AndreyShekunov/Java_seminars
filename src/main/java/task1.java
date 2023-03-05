25d00f2c13bbcca4df36edeed8eb386baf225437
Шекунов Андрей <ashekunov@gmail.com>
2023-03-05 17:26:21 +0500

    Задача 1 - Дан массив двоичных чисел, например [1,1,0,1,1,1], вывести максимальное количество подряд идущих 1.

--- src/main/java/task1.java
+++ src/main/java/task1.java
@@ -4,5 +4,26 @@ public class task1 {
         // Дан массив двоичных чисел, например [1,1,0,1,1,1],
         // вывести максимальное количество подряд идущих 1.
 
+        int[] array = {1, 1, 0, 1, 1, 1}; // создание массива явное
+
+        int curr = 0; // счетчик для посчета повторяющихся чисел
+        int best = 0; // счетчик для лучшего результата
+
+        for (int i = 0; i < array.length; i++) {
+            int a = array[i];
+            if (a == 1) {
+                curr++;
+            }else {
+                if (curr > best) {
+                    best = curr;
+                }
+                curr = 0;
+            }
+        }
+        if (curr > best) {
+            best = curr;
+        }
+
+        System.out.println(best);
     }
 }
